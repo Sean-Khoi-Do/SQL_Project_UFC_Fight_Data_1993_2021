0c6bec35173cd451d6cb9d5b9c83d275d5ff6331
Sean-Khoi-Do <sean.khoi.ngoc.do@gmail.com>
2024-03-28 23:04:21 -0400

    update

--- sql_code/1_age_advantage.sql
+++ sql_code/1_age_advantage.sql
@@ -1,12 +1,3 @@
-/*
-MMA is a highly unpredictable sport. The goal of this project is to identify trends that I will help predict fight outcomes.
-
-A few questions I hope to answer:
-    - How does Age relate to the outcome?
-    - How does Height relate to the outcome?
-    - How does Reach relate to the outcome?
-*/
-
 --How does Age relate to the outcome?
 
 SELECT
